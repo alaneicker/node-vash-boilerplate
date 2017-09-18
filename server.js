94f6bf1dcde2c7755262e7903a9971231d562975
Eicker, Alan <alan.eicker@allstate.com>
2017-09-18 11:23:21 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const port = 4000;
+const app = express();
+
+app.set('view engine', 'vash');
+app.set('views', path.join(__dirname, '/src/views'));
+app.use(express.static(__dirname + '/public'));
+
+var model = {
+  title: 'Node.js/Vash Project Boilerplate',
+  content: 'Just some random content...'
+};
+
+app.get('/', function (req, res) {
+  res.render('index', model);
+});
+
+var server = app.listen(port, function () {
+  console.log(`Node server is running on port ${port}`);
+});
